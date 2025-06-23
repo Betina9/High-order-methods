8b0859b8dac18280fecb66de0386f06b4d412517
Betina9 <ob042025@gmail.com>
2025-06-23 13:56:16 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const gold = 200; //our money
+const products = [
+  { name: "Smartphone", price: 800, category: "electronics" },
+  { name: "Headphones", price: 150, category: "electronics" },
+  { name: "Coffee Maker", price: 100, category: "home appliances" },
+  { name: "Blender", price: 250, category: "home appliances" },
+  { name: "Laptop", price: 1200, category: "electronics" },
+  { name: "T-shirt", price: 50, category: "clothing" },
+  { name: "Sneakers", price: 300, category: "clothing" },
+];
+const affordableItems = products.filter((item) => item.price <= gold);
+console.log(`With ${gold} gold, we can afford`, affordableItems);
